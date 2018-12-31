229577965a57fe889c9e9b7c24dc4cb89ba46adc
Konstantin Sokolov <kksokolo@mts.ru>
2018-12-31 20:11:50 +0300

    Check if user is in a database, if not - add with 0 points.

--- app.js
+++ app.js
@@ -30,4 +30,17 @@ client.connect();
 
 client.on('connected', (address, port) => {
     client.action(channel, 'Connected')
-})
+});
+
+client.on("message", function (channel, userstate, message, self) {
+    if (self) return;
+    if (message === '!bet') {
+        db.read()
+        if (db.get('users').find({username: userstate.username}).value()) {
+            client.action(channel, `${userstate.username} is in the base`)
+        } else {
+            client.action(channel, `${userstate.username} is not the base`)
+            db.get('users').push({username: userstate.username, point: '0'}).write()
+        }
+    }
+  });
