090a4569721372ae84ec6f59952681db7b025c7b
Konstantin Sokolov <kksokolo@mts.ru>
2019-01-01 13:02:13 +0300

    Create !start and !end broadcaster-only actions to initialize betting.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const tmi = require('tmi.js')
 const config = require('./config')
 const channel = config.testChannel;
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const rx = require('rxjs');
 
-const adapter = new FileSync('./db.json')
-const db = low(adapter)
+const adapter = new FileSync('./db.json');
+const db = low(adapter);
 
+let betting = false;
+let _pool = [];
+const pool$ = new rx.Subject(_pool);
 
-db.defaults({ users: [] }).write()
+db.defaults({ users: [] }).write();
 
 let options ={
     options: {
@@ -33,19 +37,39 @@ client.on('connected', (address, port) => {
 });
 
 client.on("message", function (channel, userstate, message, self) {
-    if (self) return;
-    if (message === '!bet') {
-        db.read()
-        if (db.get('users').find({username: userstate.username}).value()) {
-            client.action(channel, `${userstate.username} is in the base`)
-        } else {
-            client.action(channel, `${userstate.username} is not the base`)
-            db.get('users').push({username: userstate.username, point: '0'}).write()
-        }
+    let username = userstate.username;
+    switch (true) {
+        case self:
+            break;
+        case message === '!points':
+            db.read();
+            if (!isUserInDB(username)) {
+                pushNewUser(username);
+            }
+            client.action(channel, (db.get('users')
+                                      .find({username})
+                                      .value().points));
+            break;
+        case (message === '!start' && userstate.badges.broadcaster === '1' && !betting):
+            client.action(channel, `Start`);
+            betting = true;
+            pool$.subscribe( next => _pool.push(next));
+            break;
+        case (message === '!end' && userstate.badges.broadcaster === '1' && betting):
+            client.action(channel, `Stop`);
+            betting = false;
+            pool$.complete();
+            console.log(_pool);
+            break;
     }
-    if (message === '!points') {
-        db.read()
-        client.action(channel, (db.get('users')
-                                  .find({username: userstate.username})
-                                  .value().point).toString())
-}});
+
+});
+
+function isUserInDB (user) {
+    if(db.get('users').find({username: user}).value()) return true;
+    return false;
+}
+
+function pushNewUser (user) {
+    db.get('users').push({username: user, points: 0}).write();
+} 
