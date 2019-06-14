a0e090e0bcfbb9b6c1e4795615dfa17a8168e712
spnq <spnqiddqd@riseup.net>
2019-06-14 13:26:50 +0300

    After !result divide sum of all bets by all the winners and add points. Assign temporary pool array to its initial state.

--- app/app.js
+++ app/app.js
@@ -12,6 +12,21 @@ let betting = false;
 let _pool = [];
 const pool$ = new rx.Subject(_pool);
 
+pool$.subscribe(next => {
+    let targetRecord = _pool.filter(record => record.username === next.username);
+    if (targetRecord.length > 0) {
+        _pool = _pool.filter(record => record.username !== next.username)
+                     .concat(targetRecord.map(val => val = {
+                        username: next.username,
+                        bet: val.bet + next.bet,
+                        guess: next.guess
+                     }));
+    } else {
+        _pool.push(next);
+    }
+    console.log(_pool);
+});
+
 db.defaults({users: []}).write();
 
 let options = {
@@ -19,7 +34,7 @@ let options = {
         debug: true
     },
     connection: {
-        cluster: "aws",
+        cluster: 'aws',
         reconnect: true
     },
     identity: {
@@ -35,127 +50,89 @@ client.connect();
 
 client.on('connected', () => client.action(channel, 'Connected'));
 
-client.on("message", (channel, userstate, message, self) => {
-    if (self) return;
-    
-    let username = userstate.username;
-    let isAdmin = userstate.badges.broadcaster === '1';
-    let currentPoints;
+client.on('message', (channel, userstate, message, self) => {
+    if (self || !message.startsWith('!')) return;
 
-    if (message.startsWith('!bet') && betting) bet(message, username, currentPoints);
+    let username = userstate.username.toLowerCase();
+    let isAdmin = userstate.badges && userstate.badges.broadcaster && userstate.badges.broadcaster === '1';
+
+    if (message.startsWith('!bet') && betting) bet(message, username);
     if (message === '!points') points(username, channel);
-    if (message.startsWith('!add') && isAdmin) add(message, currentPoints);
-    if (message === '!startBet' && isAdmin && !betting) startBet(channel);
-    if (message === '!stopBet' && isAdmin && betting) stopBet(channel);
-    if (message.startsWith('!result') && isAdmin && !betting) currentPoints = result(message, currentPoints);
+    if (message.startsWith('!add') && isAdmin) add(message);
+    if (message === '!startBet' && isAdmin && !betting) { 
+        betting = true;
+        client.action(channel, 'Start');
+    }
+    if (message === '!stopBet' && isAdmin && betting) {
+        betting = false;
+        client.action(channel, 'Stop');
+    }
+    if (message.startsWith('!result') && isAdmin && !betting) result(message);
 });
 
-function result(message, currentPoints) {
+function result(message) {
     let result = message.split(' ')[1];
-    _pool.forEach( rec => {
-        currentPoints = db.get('users')
-        .find({ username: rec.username })
-        .value().points;
-        if (rec.guess == result) db.get('users')
-                        .find({ username: rec.username })
-                        .assign({ points: rec.points + currentPoints })
-                        .write();
+    let correctGuessedPool = [];
+    let correctGuessedSum = 0;
+    let wholePot = _pool.reduce( (acc, val) =>  {
+        if (acc.bet) return parseInt(acc.bet) + parseInt(val.bet);
+        return parseInt(acc) + parseInt(val.bet);
     })
-    return currentPoints;
+
+    correctGuessedPool = _pool.filter( record => parseInt(record.guess) === parseInt(result))
+
+    correctGuessedSum = correctGuessedPool.reduce( (acc, val) => {
+        if (acc.bet) return parseInt(acc.bet) + parseInt(val.bet);
+        return parseInt(acc) + parseInt(val.bet);
+    })
+
+    correctGuessedPool.forEach( correctGuessedUserRecord => {
+        console.log(wholePot, typeof wholePot, correctGuessedSum, typeof correctGuessedSum, correctGuessedUserRecord.bet, typeof correctGuessedUserRecord.bet)
+            let prize =  parseInt(wholePot/(correctGuessedSum/correctGuessedUserRecord.bet))
+            console.log(prize)
+            changePoints(correctGuessedUserRecord.username.toLowerCase(), prize)
+         })
+
+    _pool = [];
 }
 
-function stopBet(channel) {
-    client.action(channel, `Stop`);
-    betting = false;
-    pool$.complete();
-    console.log(_pool);
-}
-
-function startBet(channel) {
-    client.action(channel, `Start`);
-    betting = true;
-    pool$.subscribe(next => {
-        let targetRecord = _pool.filter(record => record.username === next.username);
-        if (targetRecord.length > 0) {
-            _pool = _pool.filter(record => record.username !== next.username)
-                .concat(targetRecord.map(val => val = {
-                    username: next.username,
-                    bet: val.bet + next.bet,
-                    guess: next.guess
-                }));
-        }
-        else {
-            _pool.push(next);
-        }
-        console.log(_pool);
-    });
-}
-
-function points(username, channel) {
-    db.read();
-    if (!isUserInDB(username)) pushNewUser(username);
-    client.action(channel, `${username}, ${(db.get('users')
-                                              .find({username})
-                                              .value().points)}`);
-}
-
-function isUserInDB(user) {
-    if (db.get('users')
-          .find({username: user})
-          .value()) return true;
-    return false;
-}
-
-function pushNewUser(user) {
-    db.get('users')
-      .push({username: user, points: 100})
-      .write();
-}
-
-function add(message, currentPoints) {
+function add(message) {
     if (message.split(' ').length !== 3) return;
     db.read();
-    let addToUser = message.split(' ')[1];
+    let addToUser = message.split(' ')[1].replace('@', '').toLowerCase();
     let addAmount = parseInt(message.split(' ')[2]);
     if (isNaN(addAmount) || addAmount < 0) addAmount = 0;
     if (!isUserInDB(addToUser)) pushNewUser(addToUser);
 
-    currentPoints = db.get('users')
-                      .find({username: addToUser})
-                      .value().points;
+    changePoints(addToUser, addAmount)
 
-    db.get('users')
-      .find({username: addToUser})
-      .assign({points: currentPoints + addAmount})
-      .write();
+    client.action(channel, `${addAmount} where added to ${addToUser}. Points: ${getCurrentPoints(addToUser)}`)
 }
 
-function bet(message, username, currentPoints) {
+function bet(message, username) {
     if (message.split(' ').length !== 3) return;
     let bettingAmount = parseInt(message.split(' ')[2]);
     let guess = parseInt(message.split(' ')[1]);
     if (bettingAmount < 0 || bettingAmount != message.split(' ')[2]) {
         client.action(channel, `${username} illigal bet value.`);
+        return;
     }
     if (guess < 0 || guess != message.split(' ')[1]) {
         client.action(channel, `${username} illigal guess value.`);
+        return;
     }
+
     db.read();
 
     if (!isUserInDB(username)) pushNewUser(username);
 
-    currentPoints = db.get('users')
-                      .find({username: username})
-                      .value().points;
-
-    if (bettingAmount > currentPoints) {
+    if (bettingAmount > getCurrentPoints(username)) {
         client.action(channel, `${username} don't have enougth points!`);
     } else {
-        db.get('users')
-          .find({username: username})
-          .assign({points: currentPoints - bettingAmount})
-          .write();
+        changePoints(username, -bettingAmount)
+
         client.action(channel, `${username} you just bet ${bettingAmount}.`);
+
         pool$.next({
             username: username,
             bet: bettingAmount,
@@ -163,3 +140,36 @@ function bet(message, username, currentPoints) {
         });
     }
 }
+
+function points(username, channel) {
+    db.read();
+    if (!isUserInDB(username)) pushNewUser(username);
+    client.action(channel, `${username}, ${getCurrentPoints(username)}`);
+}
+
+function isUserInDB(username) {
+    if (db.get('users').find({username: username}).value()) return true;
+    return false;
+}
+
+function changePoints(username, amount) {
+    let currentPoints = getCurrentPoints(username)
+
+    db.get('users')
+    .find({ username: username })
+    .assign({points: currentPoints + amount})
+    .write();
+}
+
+function getCurrentPoints(username) {
+    return db.get('users')
+             .find({ username: username })
+             .value().points;
+}
+
+function pushNewUser(username) {
+    db.get('users')
+      .push({username: username, points: 100})
+      .write();
+}
+
