8cdc47c243847a84a17b38ab48b2a5559fa60da9
spnq <spnqiddqd@riseup.net>
2019-06-14 14:51:44 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+exports.auth = ''; //Oauth you got from twitchapps.com/tmi
+exports.botName = ''; //Bot's nickname from twitch.com
+exports.channels = ['']; //Channels bot able to connect
+exports.testChannel = ''; //Value with a channel, using for testing purposes 
